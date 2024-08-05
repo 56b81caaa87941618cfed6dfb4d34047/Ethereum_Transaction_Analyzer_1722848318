eece3cf09ef336f3698fa7442b6a58be9969d761
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-05 08:58:48 +0000

    Placeholder text replacement

--- components/simple_footer_component_1722848320.js
+++ components/simple_footer_component_1722848320.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722848320", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            EtherAnalytics - Ethereum Transaction Insights
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Stay ahead of the Ethereum blockchain with real-time transaction monitoring and analysis. Powered by cutting-edge technology to provide you with actionable insights.
                     </div>
                 </div>
             </div>
